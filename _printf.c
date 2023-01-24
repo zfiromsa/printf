1f133ea6c3bedab71f976749c08a310c4964553b
zfiromsa <mdzm841@gmail.com>
2023-01-24 12:15:42 -0800

    go

--- _printf.c
+++ _printf.c
@@ -5,11 +5,11 @@ int _printf(const char *format, ...)
 int i, count, value;
 va_list args;
 va_start(args, format);
+int (*f)(va_list);
 
 i = 0;
 count = 0;
 value = 0;
-int (*f)(va_list);
 /*print parsint a null pointer*/
 if (format == NULL)
 {
